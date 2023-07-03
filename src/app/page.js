efc54d14798fc2061652f03a8bd912cd9af82126
Michelle wangui maina <66360345+MICH-MAINA@users.noreply.github.com>
2023-07-03 21:09:47 +0300

    results tab

--- src/app/page.js
+++ src/app/page.js
@@ -5,14 +5,18 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className='mainBox' style={{ background: "white" }}>
         <div className='results box'>
-          <p>Results</p>
+          <p style={{color:'#D3D3F5'}}>Your Result</p>
           <div className='grade'>
-            <div>76</div>
-            <div>of 100</div>
+            <div className='value1'>76 <span></span></div>
+            <div className='value2' style={{color:'#A9A9A9', fontSize:15}}>of 100</div>
+          </div>
+          <div className='gradeDes'>
+            <div><p style={{fontSize: 20, fontWeight:600}}>Great</p></div>
+            <div><p style={{color:'#D3D3F5', fontSize:15}}>You scored higher than 65% of the people who have taken this test</p></div>
           </div>
         </div>
         <div className='summary box'>
-          <p>summary</p>
+          <p>Summary</p>
         </div>
       </div>
     </main>
